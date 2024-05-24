4d340837f00b5e78e5448e8eb9ec4026367acfed
HectorLeonelPro <hector@prointernet.mx>
2024-05-24 13:37:21 -0600

    asd
    asd

--- routes/rt_routes.js
+++ routes/rt_routes.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { prueba } = require('../controllers/controllers_views/ctrl_prueba')
 
-
-
-router.get('/envio', prueba.envio)
-router.get('/resultados', prueba.resultados)
 
 
 
--- routes/rt_views.js
+++ routes/rt_views.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { prueba } = require('../controllers/controllers_views/ctrl_prueba')
 
 
 
+router.get('/envio', prueba.envio)
+router.get('/resultados', prueba.resultados)
 
 
 
