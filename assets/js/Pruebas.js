02cdccb6cfbb1eada04b4cb86e3194a50e611569
CamilaLinethPro <camila@prointernet.mx>
2024-06-06 13:37:44 -0600

    PRUEBAS
    agregue funcionamiento de fetch en todos

--- assets/js/Pruebas.js
+++ assets/js/Pruebas.js
@@ -19,7 +19,7 @@ function consultaVehiculos(e) {
         headers: settings.headers,
     }).then((response) => response.json())
         .then((data) => {
-            console.log('vehiculos', data)
+            console.log('vehiculo', data)
             let vehiculos = data
         })
         .catch((error) => {
@@ -46,7 +46,7 @@ function consultaConductor(e) {
         headers: settings.headers,
     }).then((response) => response.json())
         .then((data) => {
-            console.log('conductores', data[0])
+            console.log('conductor', data[0])
         })
         .catch((error) => {
             console.error("Error al hacer fetch de recibir:", error);
@@ -54,16 +54,14 @@ function consultaConductor(e) {
 
 }
 
-
-
 function vehiculosRuta(e) {
     e.preventDefault();
-    let id = document.getElementById('route_id').value
+    let id = document.getElementById('planned_date').value
 
     var settings = {
         async: true,
         crossDomain: true,
-        url: `https://api.simpliroute.com/v1/accounts/drivers/${id}`,
+        url: `https://api.simpliroute.com/v1/plans/${id}/vehicles/`,
         method: "GET",
         headers: {
             "content-type": "application/json",
@@ -75,7 +73,7 @@ function vehiculosRuta(e) {
         headers: settings.headers,
     }).then((response) => response.json())
         .then((data) => {
-            console.log('conductores', data[0])
+            console.log('visitas de ruta', data[0])
         })
         .catch((error) => {
             console.error("Error al hacer fetch de recibir:", error);
@@ -86,11 +84,39 @@ function vehiculosRuta(e) {
 function visitasRuta(e) {
     e.preventDefault();
     let id = document.getElementById('route_id').value
+    
 
     var settings = {
         async: true,
         crossDomain: true,
-        url: `https://api.simpliroute.com/v1/accounts/drivers/${id}`,
+        url: `https://api.simpliroute.com/v1/plans/routes/${id}/visits/`,
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+            authorization: `Token 7d52c18aaeb322b0c1a3bf9bcb481ae9ee029495`,
+        },
+    };
+    fetch(settings.url, {
+        method: `${settings.method}`,
+        headers: settings.headers,
+    }).then((response) => response.json())
+        .then((data) => {
+            console.log('ruta', data[0])
+        })
+        .catch((error) => {
+            console.error("Error al hacer fetch de recibir:", error);
+        });
+
+}
+
+function visitaInfo(e){
+    e.preventDefault();
+    let id = document.getElementById('visit_id').value
+
+    var settings = {
+        async: true,
+        crossDomain: true,
+        url: `https://api.simpliroute.com/plans/v1/visits/${id}/detail/`,
         method: "GET",
         headers: {
             "content-type": "application/json",
