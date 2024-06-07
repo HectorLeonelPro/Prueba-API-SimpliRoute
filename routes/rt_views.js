c638fd650bdf94a5ad7d354231f76852f0e55dab
HectorLeonelPro <hector@prointernet.mx>
2024-06-07 17:40:16 -0600

    Comit
    espero q jale

--- routes/rt_views.js
+++ routes/rt_views.js
@@ -9,7 +9,7 @@ router.get('/pruebas', prueba.pruebas)
 router.get('/resultados', prueba.resultados)
 router.get('/consultas', prueba.consultas)
 router.get('/consultas_prueba', prueba.consultas_prueba)
-router.get('/consultas_mapbox', prueba.consultas_mapbox)
+router.get('/consultas_nacional', prueba.consultas_plan_nacional)
 router.get('/consultas_google', prueba.consultas_google)
 
 
