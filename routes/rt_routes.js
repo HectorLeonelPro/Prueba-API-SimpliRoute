a2da5f99b2acbfaf5ea166049c9f614d53d7b23a
CamilaLinethPro <camila@prointernet.mx>
2024-06-07 17:51:33 -0600

    DIOS SOY YO DE NUEVO
    This reverts commit 88a1c88bc44e0a89d9cde513e8d3a1abed586dc8.

--- routes/rt_routes.js
+++ routes/rt_routes.js
@@ -10,4 +10,13 @@ router.post('/geolocalizacion', prueba.rt_geolocalizacion)
 router.post('/geolocalizacion_inversa', prueba.rt_geolocalizacion_inversa)
 router.get('/rt_consulta_vehiculo_conductor', prueba.rt_consulta_vehiculo_conductor)
 
+//PRUEBAS 
+router.post('/busca_vehiculo', prueba.rt_consulta_vehiculo)
+router.post('/busca_conductor', prueba.rt_consulta_conductor)
+router.post('/busca_vehiculo_ruta', prueba.rt_consulta_vehiculos_ruta)
+router.post('/busca_visitas_ruta', prueba.rt_visitas_ruta)
+router.post('/busca_visitas_info', prueba.rt_visitas_info)
+router.post('/busca_optimizacion_distancia', prueba.rt_optimizacion_distancia)
+router.post('/busca_ruta', prueba.rt_consulta_ruta)
+
 module.exports = router;
