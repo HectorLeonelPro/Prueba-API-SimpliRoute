428b7884542096103216cd62d4926d170d3bae06
CamilaLinethPro <camila@prointernet.mx>
2024-06-07 17:35:51 -0600

    Revert "Merge branch 'main' of https://github.com/HectorLeonelPro/Prueba-API-SimpliRoute"
    This reverts commit 434493fa09bc0bf09143d67e87f5a95611a65895, reversing
    changes made to 98dbf673e2ebe8cd4cd189cf5a41d4db7a5d7439.

--- routes/rt_routes.js
+++ routes/rt_routes.js
@@ -4,7 +4,6 @@ const { prueba } = require('../controllers/controllers_rt/ctrl_prueba')
 
 router.post('/envio-plan', prueba.rt_plan)
 router.post('/envio-plan-prueba', prueba.rt_plan_prueba)
-router.post('/envio-plan-nacional', prueba.rt_plan_nacional)
 router.post('/rt-cargar-sucursales', prueba.rt_consulta_sucursales)
 router.post('/geolocalizacion', prueba.rt_geolocalizacion)
 router.post('/geolocalizacion_inversa', prueba.rt_geolocalizacion_inversa)
